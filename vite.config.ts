902dbbc916a74e23f8b18666201ca7291286edab
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-09-30 23:59:00 +0530

    Refactor build target to use 'esnext' or a more recent version

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,7 @@ import wasm from 'vite-plugin-wasm'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
-  
+  build: {
+    target: 'esnext', // Change this to 'esnext' or a more recent version
+  },
 })
